4872bf3492333e98e01e66c4f2c24777319b93dd
Camilla Murgia <camilla.murgia@hellofresh.com>
2022-04-03 19:44:51 +0200

    create style for the the post overview

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Categories, PostCard, PostWidget} from '../components/index';
@@ -5,41 +6,52 @@ import { getPosts } from '../services'
 
 export interface PostInterface {
   title: string,
-  excerpt: string
+  excerpt: string,
+  featuredImage: {
+    url: string,
+  },
+  slug: string,
+  author: {
+    name: string,
+    photo: {
+      url: string,
+    },
+  },
+  createdAt: Date,
 }
 
-const Home: NextPage = ({ posts }): React.ReactElement => {
-  console.log('posts', posts);
+const Home: NextPage = ({ posts }) => {
   
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div>
       <Head>
         <title>CMS BLOG</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className='lg:col-span-8 col-span-1' >
-          {posts.map((post) => <PostCard post={post} key={post.title}/> )}
-        </div>
-
-        <div className='lg:col-span-4 col-span-1'>
-          <div className='lg:sticky relative top-8'>
-            <PostWidget />
-            <Categories />
+      <div className="container mx-auto px-10 mb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="lg:col-span-8 col-span-1">
+            {posts.map((post, index) => (
+              <PostCard key={index} post={post.node} />
+            ))}
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+            <div className="lg:sticky relative top-8">
+              <PostWidget />
+              <Categories />
+            </div>
           </div>
         </div>
-
       </div>
-      </main>
     </div>
-  )
+  );
 }
 
-export async function getStaticProps(): Promise<object> {
-  const posts = (await getPosts()) || [];
+// Fetch data at build time
+export async function getStaticProps() {
+  const posts: Record<string, unknown>[] = (await getPosts()) || [];
   return {
-    props: { posts }
+    props: { posts },
   }
 }
 
--- services/index.tsx
+++ services/index.tsx
@@ -1,12 +1,14 @@
 import { request, gql } from 'graphql-request';
 
+
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export const getPosts = async (): Promise<any> => {
+export const getPosts = async () => {
   const query = gql`
     query MyQuery {
       postsConnection {
         edges {
+          cursor
           node {
             author {
               bio
@@ -31,8 +33,9 @@ export const getPosts = async (): Promise<any> => {
         }
       }
     }
-  `
-  // @ts-ignore
+  `;
+
+  if (!graphqlAPI) return
   const result = await request(graphqlAPI, query)
 
   return result.postsConnection.edges;
